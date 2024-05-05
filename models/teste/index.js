5794d6d73b304e7f262b81c1a39bd61d1114c408
DevAnaBeatriz <ana.mb236@gmail.com>
2024-05-05 13:29:07 -0300

    models

--- models/teste/index.js
+++
@@ -1,8 +0,0 @@
-(async()=>{
-    const database = require('./models/db')
-    const Produto = require('./models/produtos')
-    const Fabricante = require('./models/fabricante')
-    await database.sync({force: true})
-
-
-})
