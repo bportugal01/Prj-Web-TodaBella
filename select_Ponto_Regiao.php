94095f5f4286dc588f26f8817e6ad3e853aa14e8
DevAnaBeatriz <ana.mb236@gmail.com>
2024-05-05 12:17:06 -0300

    pesquisas detalhas alterações

--- select_Ponto_Regiao.php
+++ select_Ponto_Regiao.php
@@ -1,61 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PontosRegioes</title>
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
-        rel="stylesheet">
-
-
-
-
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-
-    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
-
-    <link rel="stylesheet" href="assets/css/templatemo-hexashop.css">
-
-    <link rel="stylesheet" href="assets/css/owl-carousel.css">
-
-    <link rel="stylesheet" href="assets/css/lightbox.css">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular-animate.js"></script>
-    <script src="//angular-ui.github.io/bootstrap/ui-bootstrap-tpls-1.2.5.js"></script>
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
-
-
-    <link rel="stylesheet" href="assets/css/css.css">
-
-    <style>
-        /* Adicione este código ao seu arquivo de estilo CSS */
-        .logo {
-            align-items: center;
-        }
-
-
-
-        .logo img {
-            max-height: 80px;
-            /* Ajuste a altura máxima conforme necessário */
-            max-width: 100%;
-            /* Garante que o logo não ultrapasse o contêiner */
-            margin-right: 10px;
-            /* Espaçamento entre o logo e o texto (se houver) */
-         
-        }
-    </style>
-
+    <title>Pontos Estratégicos por Regiões</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-
-
-<!-- Restante do seu código HTML -->
+<body class="flex flex-col h-screen">
+    <div class="flex-grow bg-rose-50">
+        <header>
+            <nav class="bg-white outline outline-offset-2 outline-rose-700">
+                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a href="gerenciamento.php" class="flex items-center space-x-3 rtl:space-x-reverse">
+                        <img src="assets/images/logo20.png" class="h-24" alt="Logo Toda Bella" />
+                    </a>
+                    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-rose-500 rounded-lg md:hidden hover:bg-rose-100 focus:outline-none focus:ring-2 focus:ring-rose-200" aria-controls="navbar-default" aria-expanded="false">
+                        <span class="sr-only">Abrir Menu</span>
+                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                        </svg>
+                    </button>
+                    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-rose-100 rounded-lg bg-white md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+                            <li>
+                                <a href="gerenciamento.php" class="block py-2 px-3 text-rose-700 text-lg font-bold rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-400 md:p-0">Home</a>
+                            </li>
+                            <li>
+                                <a href="select.php" class="block py-2 px-3 text-rose-700 text-lg font-bold rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-400 md:p-0">Pesquisas Detalhadas</a>
+                            </li>
+                            <li>
+                                <a href="sair.php" class="block py-2 px-3 text-rose-700 text-lg font-bold rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-400 md:p-0">Sair</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>  
+            </nav>
+</head>
 
 <body>
     <!-- ***** Preloader Start ***** -->
@@ -66,121 +46,50 @@
             <div></div>
         </div>
     </div>
-    <!-- ***** Preloader End ***** -->
-
-    <header class="header-area header-sticky sticky-top">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <nav class="main-nav">
-                        <!-- ***** Logo Start ***** -->
-                        <a href="gerenciamento.php" class="logo">
-                            <img src="assets/images/logo20.png">
-                        </a>
-                        <!-- ***** Logo End ***** -->
-                        <!-- ***** Menu Start ***** -->
-                        <ul class="nav">
-                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-                            <li class="submenu">
-                                <a href="javascript:;">Cadastro</a>
-                                <ul>
-                                    <li><a href="cadastro_Cliente.php">Cadastro de Cliente</a></li>
-                                    <li><a href="cadastro_itemNotaFical.php">Cadastro de Item Nota Fiscal</a></li>
-                                    <li><a href="cadastro_NotaFiscal.php">Cadastro de Nota Fiscal</a></li>
-                                    <li><a href="cadastro_Produto.php">Cadastro de Produto</a></li>
-                                    <li><a href="cadastro_Regiao_Ponto.php">Cadastro de Região Ponto</a></li>
-                                    <li><a href="cadastro_Veiculo.php">Cadastro de Veículo</a></li>
-                                    <li><a href="cadastro_Vendedor.php">Cadastro de Vendedor</a></li>
-                                    <li><a href="cadatro_UtilizacaoVeiculo.php">Cadastro de Utilização de Veículo</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="submenu">
-                                <a href="javascript:;">Listagem</a>
-                                <ul>
-                                    <li><a href="listar_Cliente.php">Listar de Cliente</a></li>
-                                    <li><a href="listar_NotaFiscal.php">Listar de Nota Fiscal</a></li>
-                                    <li><a href="listar_Produto.php">Listar de Produto</a></li>
-                                    <li><a href="listar_RegiaoPonto.php">Listar de Região Ponto</a></li>
-                                    <li><a href="listar_Veiculo.php">Listar de Veículo</a></li>
-                                    <li><a href="listar_Vendedor.php">Listar de Vendedor</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="select.php">Pesquisas detalhadas</a></li>
-                            <li><a href="sair.php">Sair</a></li>
-                        </ul>
-                        <a class='menu-trigger'>
-                            <span>Menu</span>
-                        </a>
-                        <!-- ***** Menu End ***** -->
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </header>
     <br>
+    <div class="relative overflow-x-auto sm:rounded-lg px-5 md:px-20 pt-10">
+
     <div class="subscribe">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="border p-4">
-                    <div class="section-heading">
-                        <?php
-                        include_once 'DAO/PontoEstratagicoDAO.php';
-                        $pontos = PontoEstrategicoDAO::PontosRegioes();
-                        ?>
+    <div class="container">
+        <div class="flex justify-center ">
+            <div class="p-4">
+                <div class="section-heading">
+                    <?php
+                    include_once 'DAO/PontoEstratagicoDAO.php';
+                    $pontos = PontoEstrategicoDAO::PontosRegioes();
+                    ?>
 
-                        <h2>Lista de pontos estratégicos por região</h2>
+                    <h2 class="text-lg font-bold mb-4 text-rose-700">Lista de pontos estratégicos por região</h2>
 
-                        <table>
-                            <thead>
+                    <table class="w-full bg-white border-2 border-rose-700">
+                        <thead>
+                            <tr class="bg-rose-700 text-white">
+                                <th class="px-4 py-2">Região</th>
+                                <th class="px-4 py-2">Ponto Estratégico</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($pontos as $ponto): ?>
                                 <tr>
-                                    <th>Região</th>
-                                    <th>Ponto Estratégico</th>
+                                    <td class="border-t border-rose-700 px-4 py-2"><?= $ponto['NomeRegiao'] ?></td>
+                                    <td class="border-t border-rose-700 px-4 py-2"><?= $ponto['NomePonto'] ?></td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach ($pontos as $ponto): ?>
-                                    <tr>
-                                        <td>
-                                            <?= $ponto['NomeRegiao'] ?>
-                                        </td>
-                                        <td>
-                                            <?= $ponto['NomePonto'] ?>
-                                        </td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                        <a href="select.php" class="btn btn-primary " style="background-color: #c62152" href="select.php">voltar</a>
-                    </div>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                    <a href="select.php" class="block w-full mt-8">
+                        <div class="inline-flex items-center justify-center px-10 py-2 text-base font-medium text-white bg-rose-700 rounded-sm hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-rose-300">
+                            Voltar
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 
 
-    <!-- jQuery -->
-    <script src="assets/js/jquery-2.1.0.min.js"></script>
-
-    <!-- Bootstrap -->
-    <script src="assets/js/popper.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-
-    <!-- Plugins -->
-    <script src="assets/js/owl-carousel.js"></script>
-    <script src="assets/js/accordions.js"></script>
-    <script src="assets/js/datepicker.js"></script>
-    <script src="assets/js/scrollreveal.min.js"></script>
-    <script src="assets/js/waypoints.min.js"></script>
-    <script src="assets/js/jquery.counterup.min.js"></script>
-    <script src="assets/js/imgfix.min.js"></script>
-    <script src="assets/js/slick.js"></script>
-    <script src="assets/js/lightbox.js"></script>
-    <script src="assets/js/isotope.js"></script>
-
-    <!-- Global Init -->
-    <script src="assets/js/custom.js"></script>
 
     <script>
 
@@ -200,6 +109,15 @@
 
     </script>
 
-</body>
+        </div>
+        <footer class="bg-rose-700 shadow mt-auto">
+            <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+                <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+                <span class="block text-base text-white text-center">Copyright © 2024 | Todos os direitos reservados - Toda Bella</span>
+            </div>
+        </footer>    
 
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="assets/js/menu.js"></script>        
+    </body>
 </html>
